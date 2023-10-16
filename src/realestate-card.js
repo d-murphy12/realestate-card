e66c3280ed9169ddc70cc9398f9409f3795ac5a6
d-murphy12 <d12murph@gmail.com>
2023-10-15 20:20:21 -0400

    update

--- src/realestate-card.js
+++ src/realestate-card.js
@@ -4,10 +4,49 @@ const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
 class RealestateCard extends LitElement {
   static properties = {
-    header: { type: String },
+    image: { type: String },
+    title: { type: String },
+    description: { type: String },
+    summary: { type: String },
+    message: { type : String },
   }
 
-  //css
+  constructor() {
+    super();
+    this.image = 'https://media.istockphoto.com/id/1072291024/vector/vector-illustration-cartoon-residential-townhouse.jpg?s=612x612&w=0&k=20&c=vhYukmGJvZYdabADEakKi8W2-V47XzZ_fUW-3RRmPjE=';
+    this.title = 'Real Estate Agency';
+    this.description = 'IST 256 Pod 2';
+    this.summary = 'Open Me!';
+    this.message = 'The best agency around! Give us a call!';
+  }
+
+  render() {
+    return html`
+    <div class="buttons">
+      <button class="duplicate" @click="${this.cloneCard}">Duplicate  card</button>
+      <button class="changetxt" @click="${this.changeTitle}">Change title</button>
+      <button id="deletecard" @click="${this.deleteCard}">Delete</button>
+      <button data-toggle-btn @click="${this.toggleDetails}">Toggle details</button>
+      <button id="changebackground" @click="${this.changeBackground}">Change Background</button>
+    </div>
+    
+    <div class= "cards">
+    <div id= "card" class="card">
+      <img src="${this.image}" width="100"
+           height="100"/>
+        <h3>${this.title}</h3>
+        <h4>${this.description}</h4>
+      <details class="details">
+        <summary>${this.summary}</summary>
+        <div>
+          ${this.message}
+        </div>
+    </div>
+    </div>
+    `;
+  }
+
+
   static styles = css`
   .card {
     width: 400px;
@@ -70,35 +109,54 @@ class RealestateCard extends LitElement {
 }
   `;
 
-  constructor() {
-    super();
-    this.header = 'My app';
-  }
 
-  render() {
-    return html`
-    <div class="buttons">
-    <button class="duplicate">Duplicate  card</button>
-    <button class="changetxt">Change title</button>
-    <button id="deletecard">Delete</button>
-    <button data-toggle-btn>Toggle details</button>
-      <button id="changebackground">Change Background</button>
-    </div>
+
+  cloneCard(e) {
+    const card = this.shadowRoot.querySelector('.card');
+    const clone = card.cloneNode(true);
+    this.shadowRoot.querySelector('.cards').appendChild(clone);
+    }
+
+    changeTitle(e) {
+        let name = "Something else";
+        if (name) {
+          this.shadowRoot.querySelector('.card h3').innerText = name;
+        }
+    }
+
+    deleteCard(e) {
+        let wantsTo = confirm("Are you sure?");
+        if (wantsTo) {
+  
+            this.shadowRoot.querySelector('.card:last-child').remove();     
+
+        }
+    }
+
+    toggleDetails(e) {
+      const details = this.shadowRoot.querySelector('summary');
+
+      if (details.parentNode.getAttribute('open')) {
+          details.parentNode.removeAttribute('open');
+      }
+      else {
+    details.parentNode.setAttribute('open','open');    
+      }
+    }
+   
     
-    <div id= "card" class="card">
-      <img src="https://media.istockphoto.com/id/1072291024/vector/vector-illustration-cartoon-residential-townhouse.jpg?s=612x612&w=0&k=20&c=vhYukmGJvZYdabADEakKi8W2-V47XzZ_fUW-3RRmPjE=" width="100"
-           height="100"/>
-        <h3 >Real Estate Agency</h3>
-        <h4>IST 256 Pod 2</h4>
-      <details class="details">
-        <summary>Open me</summary>
-        <div>
-          The best agency around! Give us a call!
-        </div>
-    </div>
-    
-    `;
-  }
+    changeBackground(e) {
+      const card = this.shadowRoot.querySelector('.card');
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        card.style.backgroundColor = '#' + randomColor;
+        return randomColor;
+        }
+
+
+
+  
 }
+
+
 
 customElements.define('realestate-card', RealestateCard);
